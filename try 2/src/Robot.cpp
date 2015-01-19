8a9d3422ddad36d7998a6a2401ac09046291e47b
denchief1 <jacob@iharrand.com>
2015-01-18 19:59:02 -0500

    fix code
    C++

---
+++ try 2/src/Robot.cpp
@@ -0,0 +1,69 @@
+#include "WPILib.h"
+
+class Robot: public IterativeRobot
+{
+
+	RobotDrive myRobot; // robot drive system
+	Joystick *stick; // only joystick
+	Talon *Elevator;
+	LiveWindow *lw;
+	int autoLoopCounter;
+
+public:
+	Robot() :
+		myRobot(0, 1), // must be intialized in same order
+		stick(1),
+		Elevator(2),
+		lw(NULL),
+		autoLoopCounter(0)
+	{
+		myRobot.SetExpiration(0.1);
+	}
+
+private:
+	void RobotInit()
+	{
+		lw = LiveWindow::GetInstance();
+	}
+
+	void AutonomousInit()
+	{
+		autoLoopCounter = 0;
+	}
+
+	void AutonomousPeriodic()
+	{
+		if(autoLoopCounter < 2) //Check if we've completed 100 loops (approximately 2 seconds)
+		{
+			myRobot.Drive(-0.5, 0.0); 	// drive forwards half speed
+			autoLoopCounter++;
+			} else {
+			myRobot.Drive(0.0, 0.0); 	// stop robot
+		}
+	}
+
+	void TeleopInit()
+	{
+
+	}
+
+	void TeleopPeriodic(void)
+	{
+		myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
+		if (stick->GetTrigger == 1) //raise the elevator
+		{
+				Elevator->Set(-0.7);
+		}
+		else if (stick->GetTrigger == 0)
+		{
+				Elevator->Set(0.0);
+		}
+	}
+
+	void TestPeriodic()
+	{
+		lw->Run();
+	}
+};
+
+START_ROBOT_CLASS(Robot);
